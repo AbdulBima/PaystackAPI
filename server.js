40211a03e3ee4346155fd23a230c80067c63a2bb
Bima <abdulrahmanbima1@gmail.com>
2024-02-04 11:16:18 +0100

    PaystackApiV1

---
+++ server.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT;
+
+const FRONTEND = process.env.FRONTEND;
+const corsOptions = {
+	origin: FRONTEND,
+	optionSuccessStatus: 200,
+};
+// Paystack API key (replace with your actual Paystack API key)
+const paystackSecretKey = process.env.PAYSTAC_KEY;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post("/api/initiate-payment", async (req, res) => {
+	try {
+		const { amount, email, metadata } = req.body;
+
+		// Paystack API endpoint for initializing a transaction
+		const paystackInitiateUrl =
+			"https://api.paystack.co/transaction/initialize";
+
+		// Make request to Paystack to initialize the transaction
+		const paystackResponse = await axios.post(
+			paystackInitiateUrl,
+			{
+				email,
+				amount,
+				metadata,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${paystackSecretKey}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+
+		const authorizationUrl =
+			paystackResponse.data.data.authorization_url;
+
+		// Respond with the authorization URL to the frontend
+		res.json({ authorization_url: authorizationUrl });
+	} catch (error) {
+		console.error("Error initiating payment:", error);
+		// Handle error and respond with an appropriate status
+		res.status(500).json({
+			error: "Internal Server Error",
+		});
+	}
+});
+app.listen(port, () => {
+	console.log(
+		`Server is running at http://localhost:${port}`
+	);
+});
