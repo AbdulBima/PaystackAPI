232748f27d289a07707e733c2968d8c0833b8d4c
Bima <abdulrahmanbima1@gmail.com>
2024-02-27 00:41:17 +0100

    Add Webhook

--- server.js
+++ server.js
@@ -57,6 +57,37 @@ app.post("/api/initiate-payment", async (req, res) => {
 		});
 	}
 });
+
+
+// Paystack Webhook endpoint
+app.post("/paystack-webhook", async (req, res) => {
+  try {
+    // Verify Paystack signature (implement this based on Paystack documentation)
+
+    // Extract necessary data from Paystack webhook event
+    const { email, amount } = req.body.data;
+
+    // Make a post request to the order endpoint
+    const orderResponse = await axios.post(
+      "https://backendv2-smz4.onrender.com/api/order",
+      {
+        orderer: email,
+        order: [amount.toString()], // Assuming amount is a number, convert it to a string or adjust as needed
+      }
+    );
+
+    // Check the order response if needed
+    console.log("Order response:", orderResponse.data);
+
+    // Acknowledge receipt of Paystack webhook event
+    res.json({ status: "success" });
+  } catch (error) {
+    console.error("Error handling Paystack webhook:", error);
+    res.status(500).json({ status: "error" });
+  }
+});
+
+
 app.listen(port, () => {
 	console.log(
 		`Server is running at http://localhost:${port}`
