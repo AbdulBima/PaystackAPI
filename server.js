3c7c109773de5feeb9cd1c8bce24876236fe5c51
Bima <abdulrahmanbima1@gmail.com>
2024-02-27 06:51:39 +0100

    Update EndPoint

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post("/paystack-webhook", async (req, res) => {
 		customerEmail = webhookResponse.data.customer.email;
     // Make a post request to the order endpoint
     const orderResponse = await axios.post(
-      "https://backendv2-smz4.onrender.com/api/order", 
+      "https://backendv2-smz4.onrender.com/api/order/", 
       {
         orderer: customerEmail,
         order: [amount.toString()], // Assuming amount is a number, convert it to a string or adjust as needed
